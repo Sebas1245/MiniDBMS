d0c8b47eb1d50a9194bca90372a8fc7451b9b91c
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2021-05-02 17:49:09 -0500

    Adds login functionality reading from config file

--- client.c
+++ client.c
@@ -6,14 +6,16 @@
 #include <unistd.h>
 #define PORT 8888
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   int sock = 0, valread;
   struct sockaddr_in serv_addr;
   char *hello = "Hello from client";
   char clientReply[1024] = {0};
   char serverMsg[1024] = {0};
 
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
     printf("\n Socket creation error \n");
     return -1;
   }
@@ -22,12 +24,14 @@ int main(int argc, char const *argv[]) {
   serv_addr.sin_port = htons(PORT);
 
   // Convert IPv4 and IPv6 addresses from text to binary form
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+  {
     printf("\nInvalid address/ Address not supported \n");
     return -1;
   }
 
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  {
     printf("\nConnection Failed \n");
     return -1;
   }
@@ -35,15 +39,22 @@ int main(int argc, char const *argv[]) {
 
   // Chat with server
   int isLoggedIn = 0;
-  while (1) {
-    if (!isLoggedIn) {
+  while (1)
+  {
+    if (!isLoggedIn)
+    {
       printf("Log In with your user and password (format: user;password): ");
       scanf("%s", clientReply);
       send(sock, clientReply, sizeof(clientReply), 0);
-      if (recv(sock, serverMsg, sizeof(serverMsg), 0) > 0) {
+      if (recv(sock, serverMsg, sizeof(serverMsg), 0) > 0)
+      {
         printf("Server: %s\n", serverMsg);
         isLoggedIn = atoi(serverMsg);
-        printf("Welcome to your MiniDBMS!\n");
+        if (!isLoggedIn)
+          printf("Authentication error, please try again.\n");
+
+        else
+          printf("Welcome to your MiniDBMS!\n");
       }
       continue;
     }
@@ -61,7 +72,8 @@ int main(int argc, char const *argv[]) {
     printf("Now, type your query:\n");
     scanf("%s", clientReply);
     send(sock, clientReply, sizeof(clientReply), 0);
-    if (recv(sock, serverMsg, sizeof(serverMsg), 0) > 0) {
+    if (recv(sock, serverMsg, sizeof(serverMsg), 0) > 0)
+    {
       printf("Query was: %s\n", serverMsg);
     }
   }
--- server.c
+++ server.c
@@ -1,127 +1,172 @@
-#include <arpa/inet.h>  //inet_addr
+#include <arpa/inet.h> //inet_addr
 #include <limits.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  //strlen
+#include <string.h> //strlen
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <syslog.h>
-#include <unistd.h>  //write
+#include <unistd.h> //write
 
 char cwd[FILENAME_MAX];
 
-static void daemonize() {
-  pid_t pid;
-  /* Fork off the parent process */
-  pid = fork();
+static void daemonize()
+{
+    pid_t pid;
+    /* Fork off the parent process */
+    pid = fork();
 
-  /* An error occurred */
-  if (pid < 0) exit(EXIT_FAILURE);
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
 
-  /* Success: Let the parent terminate */
-  if (pid > 0) exit(EXIT_SUCCESS);
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
 
-  /* On success: The child process becomes session leader */
-  if (setsid() < 0) exit(EXIT_FAILURE);
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
 
-  /* Catch, ignore and handle signals */
-  /*TODO: Implement a working signal handler */
-  signal(SIGCHLD, SIG_IGN);
-  signal(SIGHUP, SIG_IGN);
+    /* Catch, ignore and handle signals */
+    /*TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
 
-  /* Fork off for the second time*/
-  pid = fork();
+    /* Fork off for the second time*/
+    pid = fork();
 
-  /* An error occurred */
-  if (pid < 0) exit(EXIT_FAILURE);
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
 
-  /* Success: Let the parent terminate */
-  if (pid > 0) exit(EXIT_SUCCESS);
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
 
-  /* Set new file permissions */
-  umask(027);
+    /* Set new file permissions */
+    umask(027);
 
-  /* Change the working directory to the root directory */
-  /* or another appropriated directory */
-  chdir(getcwd(cwd, sizeof(cwd)));
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir(getcwd(cwd, sizeof(cwd)));
 
-  /* Close all open file descriptors */
-  int x;
-  for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--) {
-    close(x);
-  }
-
-  /* Open the log file */
-  openlog("MiniDBMS", LOG_PID, LOG_DAEMON);
-}
-
-void kill_handler(int signal) {
-  syslog(LOG_NOTICE, "Server terminated.");
-  closelog();
-  exit(0);
-}
-
-int main() {
-  int socket_desc, new_socket, c;
-  struct sockaddr_in server, client;
-  char clientMsg[1024] = {0};
-  char serverReply[1024] = {0};
-
-  printf("Starting daemonize\n");
-  daemonize();
-  syslog(LOG_NOTICE, "Server daemon running.");
-
-  while (1) {
-    // TODO: Insert daemon code here.
-    // Create socket
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_desc == -1) {
-      syslog(LOG_ERR, "Could not create socket");
-      return 1;
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--)
+    {
+        close(x);
     }
 
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8888);
+    /* Open the log file */
+    openlog("MiniDBMS", LOG_PID, LOG_DAEMON);
+}
 
-    // Bind
-    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
-      syslog(LOG_ERR, "connect error");
-      return 1;
+void kill_handler(int signal)
+{
+    syslog(LOG_NOTICE, "Server terminated.");
+    closelog();
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int socket_desc, new_socket, c;
+    struct sockaddr_in server, client;
+    char clientMsg[1024] = {0};
+    char serverReply[1024] = {0};
+    char configCreds[1024];
+
+    // load config file data
+    FILE *configFile = fopen("./config.txt", "r");
+    if (configFile == NULL)
+    {
+        printf("Error opening config file");
+        exit(1);
     }
-    syslog(LOG_NOTICE, "bind done, server running");
-    listen(socket_desc, 3);
-    syslog(LOG_NOTICE, "Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
+    else
+    {
+        fgets(configCreds, sizeof(configCreds), configFile);
+        fclose(configFile);
+        printf("Read from config file %s\n", configCreds);
+    }
+    printf("Starting daemonize\n");
+    daemonize();
+    syslog(LOG_NOTICE, "Server daemon running.");
 
-    while ((new_socket = accept(socket_desc, (struct sockaddr *)&client,
-                                (socklen_t *)&c))) {
-      syslog(LOG_NOTICE, "Connection accepted");
-
-      int isLoggedIn = 0;
-      while (recv(new_socket, clientMsg, sizeof(clientMsg), 0) > 0) {
-        // TODO: Login (se recibe usuario y contrasena en formato:
-        // user;password)
-        if (!isLoggedIn) {
-          isLoggedIn = 1;
-          snprintf(serverReply, sizeof(serverReply), "%d", isLoggedIn);
-          send(new_socket, serverReply, sizeof(serverReply), 0);
-          continue;
+    while (1)
+    {
+        // TODO: Insert daemon code here.
+        // Create socket
+        socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+        if (socket_desc == -1)
+        {
+            syslog(LOG_ERR, "Could not create socket");
+            exit(1);
         }
+        syslog(LOG_NOTICE, "Socket created successfully");
+        server.sin_addr.s_addr = INADDR_ANY;
+        server.sin_family = AF_INET;
+        server.sin_port = htons(8888);
 
-        snprintf(serverReply, sizeof(serverReply), "%s", clientMsg);
-        send(new_socket, serverReply, sizeof(serverReply), 0);
-      }
+        // Bind
+        if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+        {
+            syslog(LOG_NOTICE, "connect error");
+            exit(1);
+        }
+        syslog(LOG_NOTICE, "bind done, server running");
+        listen(socket_desc, 3);
+        syslog(LOG_NOTICE, "Waiting for incoming connections...");
+        c = sizeof(struct sockaddr_in);
+
+        while ((new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c)))
+        {
+            syslog(LOG_NOTICE, "Connection accepted");
+
+            int isLoggedIn = 0;
+            while (recv(new_socket, clientMsg, sizeof(clientMsg), 0) > 0)
+            {
+                // TODO: Login (se recibe usuario y contrasena en formato:
+                // user;password)
+                syslog(LOG_NOTICE, clientMsg);
+                if (!isLoggedIn)
+                {
+                    syslog(LOG_NOTICE, "configCreds = %s", configCreds);
+                    if (strcmp(clientMsg, configCreds) == 0)
+                    {
+                        syslog(LOG_NOTICE, "config credentials match with client credentials %s = %s", clientMsg, configCreds);
+                        isLoggedIn = 1;
+                        snprintf(serverReply, sizeof(serverReply), "%d", isLoggedIn);
+                        send(new_socket, serverReply, sizeof(serverReply), 0);
+                        continue;
+                    }
+                    else
+                    {
+                        syslog(LOG_NOTICE, "config credentials DON'T match with client credentials %s != %s", clientMsg, configCreds);
+                        snprintf(serverReply, sizeof(serverReply), "%d", isLoggedIn);
+                        send(new_socket, serverReply, sizeof(serverReply), 0);
+                    }
+                }
+                else
+                {
+                    // TODO: return query results
+                    snprintf(serverReply, sizeof(serverReply), "%s", clientMsg);
+                    send(new_socket, serverReply, sizeof(serverReply), 0);
+                }
+            }
+        }
+        if (new_socket < 0)
+        {
+            syslog(LOG_ERR, "accept failed");
+            exit(1);
+        }
+        if (signal(SIGTERM, kill_handler) == SIG_ERR)
+        {
+            syslog(LOG_NOTICE, "Error terminating process.");
+            exit(1);
+        }
     }
-    if (new_socket < 0) {
-      syslog(LOG_ERR, "accept failed");
-      exit(1);
-    }
-    if (signal(SIGTERM, kill_handler) == SIG_ERR) {
-      syslog(LOG_NOTICE, "Error terminating process.");
-      exit(1);
-    }
-  }
 }
